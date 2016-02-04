77c8ea7f16a6dd534dd5ccaa5e8d7be42e6333b1
Graham Gilbert <graham@grahamgilbert.com>
2016-02-04 16:33:50 +0000

    Move file to right place

--- puppetlabs/code/environments/production/modules/macaduk_demo/manifests/outset.pp
+++ puppetlabs/code/environments/production/modules/macaduk_demo/manifests/outset.pp
@@ -1,5 +1,5 @@
 class macaduk_demo::outset {
-    package { 'outset':
+    package { 'outsetv1':
         ensure   => installed,
         provider => pkgdmg,
         source   => 'http://192.168.33.10/outset-1.0.1.pkg',
