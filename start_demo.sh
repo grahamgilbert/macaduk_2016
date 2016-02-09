66fd6f11b7a619896eb946f094c3292bc76c53e9
Graham Gilbert <graham@grahamgilbert.com>
2016-02-09 12:02:43 +0000

    That script didn't work well

--- start_demo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-vagrant destroy -f
-vagrant up puppetserver
-vagrant provision puppetserver
-vagrant up client1
-vagrant up client2
-vagrant ssh puppetserver
-vagrant rsync-auto puppetserver
